5386e15f0713b8a2e68bbb8c84d7927140471b03
aiofargo <aio.fargo.dev@gmail.com>
2025-03-27 18:56:39 -0500

    cambio base variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql2');
 
 // Configuración de la conexión a la base de datos
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'u468593065_elecciones'
-});
+
+    const connection = mysql.createConnection({
+        host: process.env. HOSTDB || 'localhost',
+        user: process.env. USERDB || 'root',
+        password: process.env. PASSWORDDB || '',
+        database: process.env. DB || 'u468593065_elecciones',
+        port: process.env. PORTDB || 3306,
+    });
+    
+
 
 // Conectar a la base de datos
 connection.connect((error) => {
